9bdef6b10be0d3f69d8cd417b6e7b19a0745feeb
RichardBanh <20131326+RichardBanh@users.noreply.github.com>
2021-05-18 21:23:30 -0700

    patient info is now editable

--- src/Components/chart/PatientInfo.jsx
+++ src/Components/chart/PatientInfo.jsx
@@ -1,19 +1,145 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 
 export const PatientInfo = (props) => {
+  const [patientInfo, setPatientInfo] = useState(props);
+  const [changeAge, setChangeA] = useState(false);
+  const [changeGender, setChangeG] = useState(false);
+  const [changeWeight, setChangeW] = useState(false);
+  const [changeHeight, setChangeH] = useState(false);
+  const [changeDr, setChangeD] = useState(false);
+  useEffect(() => {
+    setPatientInfo(props);
+  }, [props]);
   return (
-    <div className="patientinfo">
+    <div
+      className="patientinfo"
+      onClick={() => {
+        setChangeG(false);
+        setChangeA(false);
+        setChangeD(false);
+        setChangeW(false);
+        setChangeH(false);
+      }}
+    >
       <div className="tabblock">
         <div>Patient Info</div>
-        <div>Age: {props.age}</div>
-        <div>Gender: {props.gender} </div>
-        <div>Weight: {props.weight} </div>
-        <div>Height: {props.height}</div>
-        <div>Blood Type: {props.blood} </div>
-        <div>Home Doctor: {props.dr}</div>
+        {changeAge ? (
+          <input
+            value={patientInfo.age}
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+            onChange={(e) =>
+              setPatientInfo({ ...patientInfo, age: e.target.value })
+            }
+          />
+        ) : (
+          <div
+            onClick={(e) => {
+              e.stopPropagation();
+              setChangeA(!changeAge);
+            }}
+          >
+            Age: {patientInfo.age}
+          </div>
+        )}
+        {changeGender ? (
+          <input
+            value={patientInfo.gender}
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+            onChange={(e) =>
+              setPatientInfo({
+                ...patientInfo,
+                gender: e.target.value,
+              })
+            }
+          />
+        ) : (
+          <div
+            onClick={(e) => {
+              e.stopPropagation();
+              setChangeG(!changeGender);
+            }}
+          >
+            Gender: {patientInfo.gender}
+          </div>
+        )}
+        {changeWeight ? (
+          <input
+            value={patientInfo.weight}
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+            onChange={(e) =>
+              setPatientInfo({
+                ...patientInfo,
+                weight: e.target.value,
+              })
+            }
+          />
+        ) : (
+          <div
+            onClick={(e) => {
+              e.stopPropagation();
+              setChangeW(!changeWeight);
+            }}
+          >
+            Weight: {patientInfo.weight}
+          </div>
+        )}
+        {changeHeight ? (
+          <input
+            value={patientInfo.height}
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+            onChange={(e) =>
+              setPatientInfo({
+                ...patientInfo,
+                height: e.target.value,
+              })
+            }
+          />
+        ) : (
+          <div
+            onClick={(e) => {
+              e.stopPropagation();
+              setChangeH(!changeHeight);
+            }}
+          >
+            Height: {patientInfo.height}{" "}
+          </div>
+        )}
+
+        <div>Blood Type: {patientInfo.blood} </div>
+        {changeDr ? (
+          <input
+            value={patientInfo.dr}
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+            onChange={(e) =>
+              setPatientInfo({
+                ...patientInfo,
+                dr: e.target.value,
+              })
+            }
+          />
+        ) : (
+          <div
+            onClick={(e) => {
+              e.stopPropagation();
+              setChangeD(!changeDr);
+            }}
+          >
+            Home Doctor: {patientInfo.dr}{" "}
+          </div>
+        )}
       </div>
       <div>
-        <div>Account Creation Date: {props.accountcreation}</div>
+        <div>Account Creation Date: {patientInfo.accountcreation}</div>
       </div>
     </div>
   );
